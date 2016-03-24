a39b473a2ba31594f3bf3108a16007769943da62
Aleksander <kawalaaleksander@gmail.com>
2016-03-24 14:49:33 +0100

    Create frondend basing on ng-bp

---
+++ frontend/src/app/app.js
@@ -0,0 +1,57 @@
+angular.module('TermsScheduler', [
+        'ngCookies',
+        'templates-app',
+        'templates-common',
+        'ui.router'
+    ])
+
+    //http://plnkr.co/edit/tg25kr?p=preview
+
+    .config(function myAppConfig($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('login', {
+                url: "/login",
+                templateUrl: 'login/login.tpl.html',
+                controller: 'LoginController',
+                controllerAs: 'vm'
+            })
+            .state('register', {
+                url: "/register",
+                templateUrl: 'register/register.tpl.html',
+                controller: 'RegisterController',
+                controllerAs: 'vm'
+            })
+            .state('home', {
+                url: "/",
+                templateUrl: 'home/home.tpl.html',
+                controller: 'HomeController',
+                controllerAs: 'vm'
+            });
+        $urlRouterProvider.otherwise('/');
+    })
+
+    .run(function run($rootScope, $location, $cookieStore, $http) {
+        $rootScope.globals = $cookieStore.get('globals') || {};
+        if ($rootScope.globals.currentUser) {
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+        }
+
+        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+            // redirect to login page if not logged in and trying to access a restricted page
+            var restrictedPage = ['/login', '/register'].indexOf($location.path()) === -1;
+            var loggedIn = $rootScope.globals.currentUser;
+            if (restrictedPage && !loggedIn) {
+                $location.path('/login');
+            }
+        });
+    })
+
+    .controller('AppCtrl', function AppCtrl($scope, $location) {
+        $scope.pageTitle = 'a';
+        //$scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+        //    if (angular.isDefined(toState.data.pageTitle)) {
+        //        $scope.pageTitle = toState.data.pageTitle + ' | ngBoilerplate';
+        //    }
+        //});
+    });
+
