c34999cc89efaf42fa95a1d0ec1c1bf8138bb87c
Dominik Czarnota <dominik.b.czarnota@gmail.com>
2016-07-14 14:51:54 +0200

    Suppress annoying SQLALCHEMY_TRACK_MODIFICATIONS=True warning

--- backend/src/dev_config.py
+++ backend/src/dev_config.py
@@ -5,4 +5,6 @@ SECRET_KEY = os.environ.get('FLASK_SECRET_KEY', 'dev_secret_key')
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}/dev.db'.format(os.path.dirname(os.path.realpath(__file__)))
 SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 DEBUG = True
