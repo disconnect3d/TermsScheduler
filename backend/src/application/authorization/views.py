d039942c4095462049b76a09569a724bdef1441b
Dominik Czarnota <dominik.b.czarnota@gmail.com>
2016-07-14 11:43:27 +0200

    Moved `/api` url_prefix from blueprint to endpoints
    We have decided that the urls are ambiguous when the `url_prefix` is
    passed to the Blueprint - at least for devs who are not so familiar
    with Flask.

--- backend/src/application/authorization/views.py
+++ backend/src/application/authorization/views.py
@@ -6,16 +6,16 @@ from application.authorization.models import User
 from application.decorators import public_endpoint
 
 
-bp = Blueprint('api', __name__, url_prefix='/api')
+bp = Blueprint('api', __name__)
 
 
-@bp.route('/login')
+@bp.route('/api/login')
 def get_auth_token():
     token = g.user.generate_auth_token(600)
     return jsonify({'token': token.decode('ascii'), 'duration': 600})
 
 
-@bp.route('/users', methods=['POST'])
+@bp.route('/api/users', methods=['POST'])
 @public_endpoint
 def new_user():
     json_data = request.get_json()
@@ -37,7 +37,7 @@ def new_user():
             {'Location': url_for('api.get_user', id=user.id, _external=True)})
 
 
-@bp.route('/users/<int:id>')
+@bp.route('/api/users/<int:id>')
 def get_user(id):
     user = User.query.get(id)
     if not user:
